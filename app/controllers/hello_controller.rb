6bda3eaab3b703945ce1653c598a46d7d64b9ebc
[hitodeface] <[ari0703sophia@gmail.com]>
2020-10-13 23:53:11 +0900

    top page update

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -3,9 +3,11 @@ class HelloController < ApplicationController
 
     def index
         #評価４以上の投稿をランダムに１件表示する
-        @random = Post.order("RANDOM()").where("rate > 4").limit(1)
+        @random = Post.order("RANDOM()").where("rate > 3").first
     end
 
     def link
     end
+
+
 end
